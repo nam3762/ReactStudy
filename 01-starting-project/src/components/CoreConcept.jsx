874dfaf34f763585f7af7d8bc1ac815c9a007584
nam3762 <nam37623762@gmail.com>
2024-07-12 14:32:06 +0900

    2024-07-12 Day3

---
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,11 @@
+function CoreConcept({ description, title, image }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+export default CoreConcept;
