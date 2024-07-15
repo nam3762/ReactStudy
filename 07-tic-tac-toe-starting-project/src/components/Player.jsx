f0392516aa02a4eac91fd33aa93a3d83c39648b3
nam3762 <nam37623762@gmail.com>
2024-07-15 16:08:06 +0900

    2024-07-15 Day 4 Tic-Tac-Toe game finished

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol, isActive, onChangeName }) {
+  const [playerName, setPlayerName] = useState(name);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  function handleEditing() {
+    setIsEditing((editing) => !editing);
+    if (isEditing) onChangeName(symbol, playerName);
+  }
+
+  let buttonLabel = "Edit";
+
+  isEditing ? (buttonLabel = "Save") : (buttonLabel = "Edit");
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            value={playerName}
+            required
+            onChange={handleChange}
+          ></input>
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditing}>{buttonLabel}</button>
+    </li>
+  );
+}
